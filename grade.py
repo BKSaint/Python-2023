a3a51a85a3752693ebda72a140851ea804b10181
BKSaint <swalker@brooklynsteamcenter.org>
2022-11-14 10:36:33 -0500

    added comments

--- grade.py
+++ grade.py
@@ -1,12 +1,17 @@
 
-
+#This while loop is so you can convert multiple percentages to letter grades
 while True:
     
+    #Turns the input into a variable 
     grade = int(input("What is your weighted average?: "))
 
-    if grade >= 90:
+
+
+    if grade >= 90: 
+        #This if block accounts for one letter grade for anything equal to 90 or above
         letter = "A"
     elif grade < 90 and grade >= 80:
+        #These and statements go inbetween the first and second value
         letter = "B"
     elif grade < 80 and grade >= 70:
         letter = "C"
@@ -15,4 +20,6 @@ while True:
     elif grade < 60:
         letter = "F"
 
+
+    #This prints the final message that converts the percentage to a letter grade
     print("With an average of " + str(grade) + " your letter grade is: " + letter)
